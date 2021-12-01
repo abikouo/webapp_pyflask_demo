a00445fb7a8e7f439b1c09b7e34d3d27c80b3292
abikouo <abikouo@redhat.com>
2021-12-01 16:30:14 +0100

    containerized application

--- pyapp/__init__.py
+++ pyapp/__init__.py
@@ -9,9 +9,13 @@ def create_app():
 
     # Read application configuration
     params = {}
+    containerized = False
     for env_var in appinit.required_env_vars:
         if not env_var in os.environ:
-            raise ValueError("missing {0} from environment, unable to start server.".format(env_var))
+            if env_var in ("WORKER_HOSTNAME", "WORKERS_HOSTS"):
+                containerized = True
+            else:
+                raise ValueError("missing {0} from environment, unable to start server.".format(env_var))
         params[env_var] = os.environ[env_var]
 
     app.config.from_mapping(**params)
@@ -21,7 +25,8 @@ def create_app():
     appinit.init_app(app)
 
     app.register_blueprint(auth.blue_print)
-    app.register_blueprint(infra.blue_print)
+    if not containerized:
+        app.register_blueprint(infra.blue_print)
 
     app.add_url_rule("/", endpoint="index")
 
